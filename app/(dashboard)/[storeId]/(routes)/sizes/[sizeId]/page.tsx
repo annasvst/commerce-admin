323c28fb80b67647f79f20187f99a0b10daa6828
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-07-28 14:42:24 +0200

    Add size management functionality with CRUD operations and UI components

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,29 @@
+
+import prismadb from "@/lib/prismadb";
+import { SizeForm } from "./components/size-form";
+
+// SizePage bileşeni bir Server Component'tir ve async olarak tanımlanmıştır.
+const SizePage = async ({
+  params,
+}: {
+  params: { sizeId: string }; // Dinamik rota parametreleri
+}) => {
+  // Veritabanından, verilen billboardId'ye sahip billboard'ı bul.
+  // params.billboardId'ye doğrudan erişim, fonksiyon async olduğu için güvenli.
+  const size = await prismadb.size.findUnique({
+    where: {
+      id: params.sizeId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        {/* billboard objesini initialData prop'u olarak BillboardForm'a iletiyoruz */}
+        <SizeForm initialData={size} />
+      </div>
+    </div>
+  );
+};
+
+export default SizePage;
