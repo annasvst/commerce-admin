0e623135ad7e11bf51fc0c037d63eeead04cf391
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-07-28 15:26:16 +0200

    Implement color management with CRUD operations and UI components

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,29 @@
+
+import prismadb from "@/lib/prismadb";
+import { ColorForm } from "./components/color-form";
+
+// SizePage bileşeni bir Server Component'tir ve async olarak tanımlanmıştır.
+const ColorPage = async ({
+  params,
+}: {
+  params: { colorId: string }; // Dinamik rota parametreleri
+}) => {
+  // Veritabanından, verilen billboardId'ye sahip billboard'ı bul.
+  // params.billboardId'ye doğrudan erişim, fonksiyon async olduğu için güvenli.
+  const color = await prismadb.color.findUnique({
+    where: {
+      id: params.colorId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        {/* billboard objesini initialData prop'u olarak BillboardForm'a iletiyoruz */}
+        <ColorForm initialData={color} />
+      </div>
+    </div>
+  );
+};
+
+export default ColorPage;
