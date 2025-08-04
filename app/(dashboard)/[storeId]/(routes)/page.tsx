6712c95ff98503fc6d2041817002d2518eea8678
Busra-Demirkesen <busrademirkesen2@gmail.com>
2025-08-04 13:02:27 +0200

    Add recharts dependency and implement sales and stock count actions; enhance dashboard with revenue, sales, and stock metrics

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,13 @@
 
-import { auth } from "@clerk/nextjs/server"; 
-import { redirect } from "next/navigation"; 
-import prismadb from "@/lib/prismadb"; 
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { CreditCard, DollarSign, Package } from "lucide-react";
+import { CardTitle,Card, CardHeader, CardContent } from "@/components/ui/card";
+import { formatter } from '@/lib/utils';
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { Overview } from "@/components/overview";
 
 interface DashboardPageProps {
   params: { storeId: string }; 
@@ -10,30 +16,72 @@ interface DashboardPageProps {
 
 const DashboardPage = async ({ params }: DashboardPageProps) => {
   
-  const { userId } = await auth();
-
-  
-  if (!userId) {
-    redirect("/sign-in");
-  }
-
-  
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId, // params.storeId'ye doğrudan erişim, fonksiyon async olduğu için güvenli.
-      userId, // Mağazanın oturum açmış kullanıcıya ait olduğundan emin ol.
-    },
-  });
-
-  // Eğer mağaza bulunamazsa (geçersiz ID veya kullanıcıya ait değilse), ana sayfaya yönlendir.
-  if (!store) {
-    redirect("/"); // Veya özel bir hata sayfasına yönlendirilebilir.
-  }
+const totalRevenue = await getTotalRevenue(params.storeId);
+const salesCount = await getSalesCount(params.storeId);
+const stockCount = await getStockCount(params.storeId);
 
   return (
-    <div>
-      {/* Mağaza adı dinamik olarak gösteriliyor. */}
-      Active Store: {store.name}
+    <div className="flex-col">
+    
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store"/>
+        <Separator/>
+        <div className="grid gap-4 grid-cols-3 ">
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 bp-2'>
+             <CardTitle className='text-sm font-medium'>
+             Total Revenue
+             </CardTitle>              
+              <DollarSign className="h-4 w-4 text-muted-foreground"/>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+
+
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 bp-2'>
+             <CardTitle className='text-sm font-medium'>
+             Sales
+             </CardTitle>              
+              <CreditCard className="h-4 w-4 text-muted-foreground"/>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+              +{salesCount}
+              </div>
+            </CardContent>
+          </Card>
+
+
+          <Card>
+            <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+             <CardTitle className='text-sm font-medium'>
+             Product in Stock
+             </CardTitle>              
+              <Package className="h-4 w-4 text-muted-foreground"/>
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {stockCount}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>
+              Overview
+            </CardTitle>
+          </CardHeader>
+        <CardContent className="pl-2">
+          <Overview data={[]}/> 
+        </CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
